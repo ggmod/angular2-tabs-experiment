71cefe911c12119d8c3a6176eae925c10aa89342
angular-cli <angular-cli@angular.io>
2015-10-23 18:09:48 +0200

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {Angular2TabsApp} from './app/angular2-tabs';
+
+
+bootstrap(Angular2TabsApp);
---
+++ src/app/angular2-tabs.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'angular2-tabs-app',
+  providers: [],
+  templateUrl: 'app/angular2-tabs.html',
+  directives: [],
+  pipes: []
+})
+export class Angular2TabsApp {
+
+  constructor() {}
+
+}
